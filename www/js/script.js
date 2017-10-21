8cde8e4aef4faeaabd869353df66ebe4a8c7f477
Konstantin <oknroll@gmail.com>
2017-10-21 02:38:51 +0200

    no message

--- www/js/script.js
+++ www/js/script.js
@@ -1,7 +1,10 @@
 $(function() {
 	$('.play').click(function(event) {
-		$('.main .bottom').addClass('hide');
 		$('.howitworks').height('auto').html('<script src="https://fast.wistia.com/embed/medias/7irviqctbk.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_7irviqctbk videoFoam=true" style="height:100%;width:100%">&nbsp;</div></div></div>');		
+	});
+
+	$('.member').click(function(event) {
+		return false;
 	});
 
 	brandsScroll();
